545d012dd97160b5f4d9c5d851d3ad1247746fbb
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-03-21 13:32:34 +0530

    Add : Product Review Management - Retrieve only productId and review from the list for all records. - use select LINQ Operator.

---
+++ ProductReviewManagement.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    public class ProductReviewManagement
+    {
+       
+        public static void RetreiveTop3Records(List<ProductReview> list)
+        {
+            Console.WriteLine("\nUsing method Syntax");
+            List<ProductReview> sortedInDesending = list.OrderByDescending(p => p.Rating).Take(3).ToList();
+            Program.DisplayProductReviews(sortedInDesending);
+        }
+
+     
+        public static void FetchRecordsBasedOnRatingAndProductId(List<ProductReview> list)
+        {
+            List<ProductReview> result = list.Where(p => p.Rating > 3 && p.ProductId == 1 || p.ProductId == 4 || p.ProductId == 9).ToList();
+            Program.DisplayProductReviews(result);
+        }
+
+        public static void FindingEachCountOfProductId(List<ProductReview> list)
+        {
+            var result = list.GroupBy(p => p.ProductId).Select(p => new { Id = p.Key, count = p.Count() }).ToList();
+            foreach (var item in result)
+            {
+                Console.WriteLine("ProductId: " + item.Id + " ->  " + "Count: " + item.count);
+            }
+        }
+
+      
+        public static void DisplayProductIdAndReview(List<ProductReview> list)
+        {
+            var result = list.Select(p => new { Id = p.ProductId, Review = p.Review }).ToList();
+            foreach (var item in result)
+            {
+                Console.WriteLine("ProductId: " + item.Id + " ->  " + "Review: " + item.Review);
+            }
+        }
+    }
+}
